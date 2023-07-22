dbd8de5ce740ee581cd33fb41ae2aa567df9e4a1
주봉 정 <wnqhdqhd123@gmail.com>
2023-07-23 01:06:14 +0900

    프로그래머스 lv.2 - 최대값과 최소값 - Jaden Case 문자열 만들기

---
+++ Algoritm/Programmers/JadenCase.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Metrics;
+using System.IO.Pipes;
+using System.Text.RegularExpressions;
+
+namespace Algoritm.Programmers
+{
+    public class JadenCase
+    {
+        public string solution(string s)
+        {
+            string answer = "";
+
+            string[] words = s.Split(' ');
+            foreach (string word in words)
+            {
+                for(int i = 0; i < word.Length; i++)
+                {
+                    Regex regex = new Regex("^[0-9]*$");
+                    bool isNumber = regex.IsMatch(word[i].ToString());
+                    if (isNumber)
+                        answer += word[i].ToString();
+                    else
+                        answer += i==0 ? word[i].ToString().ToUpper() : word[i].ToString().ToLower();
+                }
+
+
+                answer += " ";
+            }
+
+            
+
+            return answer.Remove(answer.Length - 1, 1);
+        }
+    }
+}
---
+++ Algoritm/Programmers/MaxMin.cs
@@ -0,0 +1,50 @@
+﻿namespace Algoritm.Programmers
+{
+    public class MaxMin
+    {
+        public string solution(string s)
+        {
+            string answer = "";
+
+            Stack<int> max = new Stack<int>();
+            Stack<int> min = new Stack<int>();
+
+            foreach (string number in s.Split(' '))
+            {
+                int num = int.Parse(number);
+                
+
+                if(max.TryPeek(out int maxNum))
+                {
+                    if(maxNum < num)
+                    {
+                        max.Push(num);
+                    }
+                }
+                else
+                {
+                    max.Push(num);
+                }
+
+                if(min.TryPeek(out int minNum))
+                {
+                    if(minNum > num)
+                    {
+                        min.Push(num);
+                    }
+                }
+                else
+                {
+                    min.Push(num);
+                }
+            };
+
+            answer += min.Peek().ToString();
+            answer += " ";
+            answer += max.Peek().ToString();
+
+
+            return answer;
+        }
+    }
+}
