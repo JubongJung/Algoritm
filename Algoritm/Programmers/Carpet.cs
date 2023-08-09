d687227d09a81d031d3380b3e84af9c1a79be6f8
JubongJung <wnqhdqhd123@gmail.com>
2023-08-09 22:02:06 +0900

    프로그래머스 lv2 - 카펫

---
+++ Algoritm/Programmers/Carpet.cs
@@ -0,0 +1,50 @@
+﻿namespace Algoritm.Programmers
+{
+    public class Carpet
+    {
+        public int[] solution(int brown, int yellow)
+        {
+            int[] answer = new int[] { };
+
+            List<int[]> commonFactors = GetCommonFactor(brown + yellow);
+            foreach(var row in commonFactors)
+            {
+                int border = row[0] * 2 + row[1] * 2 - 4;
+                if(border == brown)
+                {
+                    answer = new int[] { row[1], row[0] };
+                    break;
+                }
+            }
+
+            return answer;
+        }
+
+        public List<int[]> GetCommonFactor(int number)
+        {
+            List<int[]> result = new List<int[]>();
+            int count = 1;
+
+            int root = Convert.ToInt32(Math.Sqrt(number));
+            Console.WriteLine(root);
+
+            while (true)
+            {
+                if(number % count == 0)
+                {
+                    int left = count;
+                    int right = number / count;
+                    result.Add(new int[] { left, right });
+                }
+
+                count++;
+
+                if(count > root)
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
--- Algoritm/Programmers/Fibonacci.cs
+++ Algoritm/Programmers/Fibonacci.cs
@@ -12,20 +12,14 @@
             F[0] = 0;
             F[1] = 1;
 
-            answer = GetF(n);
+            for(int i = 2; i <= n; i++)
+            {
+                F[i] = (F[i - 1] + F[i-2]) % 1234567;
+            }
+
+            answer = F[n];
 
             return answer;
-        }
-
-        public int GetF(int n)
-        {
-            if (n < 0) return 0;
-
-            if (F[n] > 0) return F[n];
-
-            F[n] = GetF(n - 1) + GetF(n - 2);
-
-            return F[n] % 1234567;
         }
     }
 }
