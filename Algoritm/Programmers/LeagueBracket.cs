5dd7a4d33cf15bd8072a812b8f15fb4b1b4c5428
주봉 정 <wnqhdqhd123@gmail.com>
2023-08-13 12:12:08 +0900

    프로그래머스 lv2 - 예상 대진표

---
+++ Algoritm/Programmers/LeagueBracket.cs
@@ -0,0 +1,38 @@
+﻿namespace Algoritm.Programmers
+{
+    public class LeagueBracket
+    {
+        public int solution(int n, int a, int b)
+        {
+            int answer = 1;
+            while (!(a < b ? IsLeftA(a,b) : IsLeftB(a,b)))
+            {
+                a = NextNumber(a);
+                b = NextNumber(b);
+                Console.WriteLine($"a:{a}, b:{b}, count:{answer}");
+                answer++;
+                if(answer == 50)
+                {
+                    break;
+                }
+            }
+
+            return answer;
+        }
+
+        public int NextNumber(int a)
+        {
+            return a / 2 + a % 2;
+        }
+
+        public bool IsLeftA(int a, int b)
+        {
+            return a % 2 == 1 && a + 1 == b;
+        }
+
+        public bool IsLeftB(int a, int b)
+        {
+            return b % 2 == 1 && b + 1 == a;
+        }
+    }
+}
---
+++ AlgoritmTest/Programmers/LeagueBracketTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Algoritm.Programmers.Tests
+{
+    [TestClass()]
+    public class LeagueBracketTests
+    {
+        [TestMethod()]
+        [DataRow(8, 4, 7, 3)]
+        [DataRow(8, 7, 4, 3)]
+        public void solutionTest(int n, int a, int b, int answer)
+        {
+            LeagueBracket leagueBracket = new LeagueBracket();
+            Assert.AreEqual(answer, leagueBracket.solution(n, a, b));
+        }
+    }
+}
