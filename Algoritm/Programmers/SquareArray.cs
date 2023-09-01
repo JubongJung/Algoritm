4319432f9f685c60e6bfea367dfc315c0eeb5544
JubongJung <wnqhdqhd123@gmail.com>
2023-09-01 18:46:04 +0900

    프로그래머스 lv2 n^2 배열 자르기

---
+++ Algoritm/Programmers/SquareArray.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Pipes;
+
+namespace Algoritm.Programmers
+{
+    /// <summary>
+    /// https://school.programmers.co.kr/learn/courses/30/lessons/87390
+    /// </summary>
+    public class SquareArray
+    {
+        public int[] solution(int n, long left, long right)
+        {
+            int[] answer = new int[right - left + 1];
+
+            for(long i = left; i <= right; i++)
+            {
+                answer[i - left] = Convert.ToInt32(GetNumber(n, i));
+            }
+
+            return answer;
+        }
+
+        public long GetNumber(int n, long index)
+        {
+            long line = index / n + 1; // aka start number
+            long lineIndex = index % n + 1;
+            
+            if(lineIndex > line)
+            {
+                return lineIndex;
+            }
+            else
+            {
+                return line;
+            }
+        }
+    }
+}
---
+++ AlgoritmTest/Programmers/SquareArrayTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+
+namespace Algoritm.Programmers.Tests
+{
+    [TestClass()]
+    public class SquareArrayTests
+    {
+        private static SquareArray sa = new();
+
+        [TestMethod()]
+        [DataRow(3,2,5, new int[] { 3, 2, 2, 3 })]
+        [DataRow(4, 7, 14, new int[] { 4, 3, 3, 3, 4, 4, 4, 4 })]
+        public void solutionTest(int n, long left, long right, int[] result)
+        {
+            string solution = string.Join(',', sa.solution(n, left, right));
+            string expect = string.Join(',', result);
+
+            Console.WriteLine(solution);
+            Console.WriteLine(expect);
+        }
+
+        [TestMethod()]
+        [DataRow(3,2,3)]
+        [DataRow(4, 5, 2)]
+        [DataRow(4, 6, 3)]
+        [DataRow(4, 7, 4)]
+        [DataRow(4, 8, 3)]
+        [DataRow(4, 9, 3)]
+        public void GetNumberTest(int n, long index, int result)
+        {
+            Assert.AreEqual(result, sa.GetNumber(n, index));
+        }
+    }
+}
