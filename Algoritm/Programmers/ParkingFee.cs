4936f4af46a16350ed35c68ffa1dbeb89b4ff5c4
JubongJung <wnqhdqhd123@gmail.com>
2023-09-10 19:49:34 +0900

    프로그래머스 lv2 주차요금 계산

---
+++ Algoritm/Programmers/ParkingFee.cs
@@ -0,0 +1,95 @@
+﻿namespace Algoritm.Programmers
+{
+    public class ParkingFee
+    {
+        public int[] solution(int[] fees, string[] records)
+        {
+            
+
+            Dictionary<int, Record> recordsList = new Dictionary<int, Record>();
+
+            foreach (var item in records)
+            {
+                var splitedItem = item.Split(' ');
+
+                int Number = Convert.ToInt32(splitedItem[1]);
+                string time = splitedItem[0];
+                string InOut = splitedItem[2];
+
+                if(!recordsList.ContainsKey(Number))
+                {
+                    Record record = new Record(fees);
+                    recordsList.Add(Number, record);
+                }
+
+                if (InOut == "IN")
+                {
+                    recordsList[Number].InMinute = GetMinute(time);
+                }
+                else
+                {
+                    recordsList[Number].OutMinute = GetMinute(time);
+                    recordsList[Number].Minute += recordsList[Number].OutMinute - recordsList[Number].InMinute;
+                }
+            }
+
+            foreach(var item in recordsList)
+            {
+                if(item.Value.OutMinute < item.Value.InMinute)
+                {
+                    item.Value.Minute += 1439 - item.Value.InMinute;
+                }
+            }
+
+            int[] answer = new int[recordsList.Count];
+            int idx = 0;
+            foreach (var row in recordsList.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{row.Key}, {row.Value.Fee}");
+                answer[idx] = row.Value.Fee;
+                idx++;
+            }
+
+            return answer;
+        }
+
+        public int GetMinute(string time)
+        {
+            string[] splitedTime = time.Split(':');
+
+            int minute = Convert.ToInt32(splitedTime[0]) * 60 + Convert.ToInt32(splitedTime[1]);
+            return minute;
+        }
+    }
+
+   
+
+    public class Record
+    {
+        public int BasicTime { get; set; }
+        public int BasicFee { get; set; }
+        public int UnitTime { get; set; }
+        public int UnitFee { get; set; }
+        public Record(int[] fees)
+        {
+            BasicTime = fees[0];
+            BasicFee = fees[1];
+            UnitTime = fees[2];
+            UnitFee = fees[3];
+        }
+        public int InMinute { get; set; }
+        public int OutMinute { get; set; }
+        public int Minute { get; set; }
+        public int Fee { get
+            {
+                if(Minute < BasicTime) return BasicFee;
+
+                decimal times = Convert.ToDecimal((Minute - BasicTime));
+                decimal result = Math.Ceiling( times / UnitTime) * UnitFee;
+
+                return Convert.ToInt32(result) + BasicFee;
+            } 
+        }
+    }
+    
+}
---
+++ AlgoritmTest/Programmers/ParkingFeeTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algoritm.Programmers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm.Programmers.Tests
+{
+    [TestClass()]
+    public class ParkingFeeTests
+    {
+        [TestMethod()]
+        public void solutionTest()
+        {
+            ParkingFee parkingFee = new ParkingFee();
+            parkingFee.solution(new int[] { 180, 5000, 10, 600 },
+                new string[] { "05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT" });
+        }
+    }
+}
