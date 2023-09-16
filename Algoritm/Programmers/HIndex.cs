158b2a5df876b84d501568af284852b92a6c148c
JubongJung <wnqhdqhd123@gmail.com>
2023-09-16 14:04:25 +0900

    프로그래머스 lv2 H-Index

---
+++ Algoritm/Programmers/HIndex.cs
@@ -0,0 +1,33 @@
+﻿namespace Algoritm.Programmers
+{
+    public class HIndex
+    {
+        public int solution(int[] citations)
+        {
+            int answer = 0;
+
+            int[] orderedC = citations.OrderByDescending(x => x).ToArray();
+
+            for(int number = citations.Count(); number > 0; number--)
+            {
+                int count = 0;
+
+                foreach (int citation in orderedC)
+                {
+                    if (number <= citation)
+                    {
+                        count++;
+                        //break;
+                    }
+                }
+
+                if (count >= number)
+                {
+                    answer = number; break;
+                }
+            }
+            
+            return answer;
+        }
+    }
+}
---
+++ AlgoritmTest/Programmers/HIndexTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algoritm.Programmers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm.Programmers.Tests
+{
+    [TestClass()]
+    public class HIndexTests
+    {
+        [TestMethod()]
+        public void solutionTest()
+        {
+            HIndex hIndex = new HIndex();
+            Console.WriteLine(hIndex.solution(new int[] { 25, 8, 5, 3, 3 }));
+        }
+    }
+}
