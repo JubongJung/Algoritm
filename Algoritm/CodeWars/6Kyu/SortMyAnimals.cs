f44d983db407a399ffee3f01eda9c7b9b1b437ab
JubongJung <wnqhdqhd123@gmail.com>
2023-10-15 13:57:07 +0900

    6kyu :2 - SortMyAnimals - StopgninnipSMysdroW

---
+++ Algoritm/CodeWars/6Kyu/SortMyAnimals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    public class Animal
+    {
+        public string Name { get; set; } = "";
+        public int NumberOfLegs { get; set; }
+    }
+
+    /// <summary>
+    /// https://www.codewars.com/kata/58ff1c8b13b001a5a50005b4/train/csharp
+    /// </summary>
+    public class AnimalSorter
+    {
+        public List<Animal> Sort(List<Animal> input)
+        {
+            return input.OrderBy(x=>x.NumberOfLegs).ThenBy(x=>x.Name).ToList();
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/StopgninnipSMysdroW.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    public class StopgninnipSMysdroW
+    {
+        public string SpinWords(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 5)
+                {
+                    StringBuilder sb = new StringBuilder();   
+
+                    for(int j = 0; j < words[i].Length; j++)
+                    {
+                        int index = words[i].Length - j - 1;
+                        sb.Append(words[i][index]);
+                    }
+
+                    words[i] = sb.ToString();
+                }
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
