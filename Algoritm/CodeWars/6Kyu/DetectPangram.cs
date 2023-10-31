45e9b0614db074ccfe86b30671a53f018344c832
JubongJung <wnqhdqhd123@gmail.com>
2023-10-31 23:34:53 +0900

    6kyu :2 - DetectPangram - Who Likes It

---
+++ Algoritm/CodeWars/6Kyu/DetectPangram.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/545cedaa9943f7fe7b000048/csharp
+    /// </summary>
+    public class DetectPangram
+    {
+        private static int ALPHABET_COUNT = 26;
+
+        public static bool IsPangram(string str)
+        {
+            List<string> chars = new List<string>();
+            foreach(char c in str)
+            {
+                string letter = c.ToString().ToLower();
+
+                if (Regex.IsMatch(letter, "^[a-z]") && !chars.Contains(letter))
+                {
+                    chars.Add(letter);
+                }
+            }
+
+            return chars.Count == ALPHABET_COUNT;
+        }
+    }
+}
---
+++ AlgoritmTest/CodeWars/6Kyu/DetectPangramTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Algoritm.CodeWars._6Kyu.Tests
+{
+    [TestClass()]
+    public class DetectPangramTests
+    {
+        [TestMethod()]
+        [DataRow("The quick brown fox jumps over the lazy dog.!@#", true)]
+        public void IsPangramTest(string str, bool expect)
+        {
+            Assert.AreEqual(expect, DetectPangram.IsPangram(str));
+        }
+    }
+}
