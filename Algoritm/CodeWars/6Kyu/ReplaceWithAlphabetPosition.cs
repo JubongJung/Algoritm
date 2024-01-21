ed9e8b1b4af30abbb5cd6d03f7c1f5892697589b
JungJubong <wnqhdqhd123@gmail.com>
2024-01-21 11:03:02 +0900

    6kyu:2 - ReplaceWithAlphabetPositorion - TakeTenMinutesWalk

---
+++ Algoritm/CodeWars/6Kyu/ReplaceWithAlphabetPosition.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/546f922b54af40e1e90001da/train/csharp
+    /// </summary>
+    public class ReplaceWithAlphabetPosition
+    {
+        public static string Run(string text)
+        {
+            List<int> list = new List<int>();
+
+            foreach(var item in text)
+            {
+                string letter = item.ToString().ToUpper();
+                if(Regex.IsMatch(letter, "^[A-Z]*$"))
+                {
+                    int value = Convert.ToChar(letter) - 64;
+                    if(value <= 0)
+                    {
+                        value += 64;
+                    }
+                    list.Add(value);
+                }
+            }
+
+            string result = string.Join(' ', list);
+            Console.Write(result);
+            return result;
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/TakeTenMinutesWalk.cs
@@ -0,0 +1,39 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/54da539698b8a2ad76000228/csharp
+    /// </summary>
+    public class TakeTenMinutesWalk
+    {
+        public static bool Run(string[] walk)
+        {
+            int ns = 0;
+            int we = 0;
+
+            foreach(string s in walk)
+            {
+                switch(s)
+                {
+                    case "s": 
+                        ns++;
+                         break;
+
+                    case "n":
+                        ns--;
+                        break;
+
+                    case "w":
+                        we++;
+                        break;
+
+                    case "e":
+                        we--;
+                        break;
+                }
+            }
+
+            return ns == 0 && we == 0 && walk.Length == 10;
+
+        }
+    }
+}
