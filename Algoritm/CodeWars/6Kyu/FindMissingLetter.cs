eda20bf62a09a5632fb326577b10494b52012920
JungJubong <wnqhdqhd123@gmail.com>
2024-01-28 17:15:30 +0900

    6kyu: 2 - FindMissingLetter - SumOfParts

---
+++ Algoritm/CodeWars/6Kyu/FindMissingLetter.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/5839edaa6754d6fec10000a2/csharp
+    /// </summary>
+    public class FindMissingLetter
+    {
+        public static char Run(char[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i-0] + 1 != array[i])
+                {
+                    return Convert.ToChar(array[i-1] + 1);
+                }
+            }
+
+            return ' ';
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/SumsOfParts.cs
@@ -0,0 +1,23 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/5ce399e0047a45001c853c2b/csharp
+    /// </summary>
+    public class SumsOfParts
+    {
+        public static int[] PartsSums(int[] ls)
+        {
+            int sum = ls.Sum(x=> x);
+
+            List<int> ints = new List<int>();
+            ints.Add(sum);
+            foreach(var row in ls)
+            {
+                sum -= row;
+                ints.Add(sum);
+            }
+
+            return ints.ToArray();
+        }
+    }
+}
