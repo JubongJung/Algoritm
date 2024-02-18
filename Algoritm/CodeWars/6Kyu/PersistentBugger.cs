178ec2459287526c9fbb7fe54145b1a22c2fe697
JungJubong <wnqhdqhd123@gmail.com>
2024-02-18 21:53:20 +0900

    6kyu:2 - PersistentBugger - TribonacciTests

---
+++ Algoritm/CodeWars/6Kyu/PersistentBugger.cs
@@ -0,0 +1,36 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class PersistentBugger
+    {
+        public static int Run(long n)
+        {
+            char[] ints = n.ToString().ToCharArray();
+
+            int result = 0;
+
+            if (ints.Length == 1) return 0;
+
+            int tmp = 1;
+
+            foreach (char c in ints)
+            {
+                tmp *= Convert.ToInt32(c.ToString());
+                
+            }
+            result++;
+
+            while (tmp.ToString().Length > 1)
+            {
+                ints = tmp.ToString().ToCharArray();
+                tmp = 1;
+                foreach (char c in ints)
+                {
+                    tmp *= Convert.ToInt32(c.ToString());
+                }
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/Tribonacci.cs
@@ -0,0 +1,26 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class Tribonacci
+    {
+        public static double[] Run(double[] signature, int n)
+        {
+            List<double> result = signature.ToList();
+            Queue<double> queue = new Queue<double>();
+
+            foreach(var c in result.TakeLast(3))
+            {
+                queue.Enqueue(c);
+            }
+
+            for(int i = 0; i < n - 3; i++)
+            {
+                double sum = queue.Sum();
+                queue.Enqueue(sum);
+                result.Add(sum);
+                queue.Dequeue();
+            }
+
+            return result.ToArray();
+        }
+    }
+}
