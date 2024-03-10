46b2d29c028ca7caaa7e302bbefebe5918d1689c
JungJubong <wnqhdqhd123@gmail.com>
2024-03-10 19:57:57 +0900

    6kyu:2 - SortTheOdd - SplitString

---
+++ Algoritm/CodeWars/6Kyu/SortTheOdd.cs
@@ -0,0 +1,28 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class SortTheOdd
+    {
+        public static int[] Run(int[] array)
+        {
+            List<int> oddValueList = new List<int>();
+            List<int> oddIndexList = new List<int>();
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 1)
+                {
+                    oddIndexList.Add(i);
+                    oddValueList.Add(array[i]);
+                }
+            }
+
+            oddValueList = oddValueList.OrderBy(x => x).ToList();
+
+            for(int i = 0; i < oddIndexList.Count; i++)            
+            {
+                array[oddIndexList[i]] = oddValueList[i];
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/SplitString.cs
@@ -0,0 +1,23 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class SplitString
+    {
+        public static string[] Run(string str)
+        {
+            bool isOdd = str.Length % 2 == 1;
+            if (isOdd) 
+            {
+                str += "_";
+            };
+
+            List<string> result = new List<string>();
+            for(int i = 0; i < str.Length / 2; i++)
+            {
+                int index = i * 2;
+                result.Add(str[index].ToString() + str[index + 1].ToString());
+            }
+
+            return result.ToArray();
+        }
+    }
+}
