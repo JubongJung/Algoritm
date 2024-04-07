1ba5212d437baf57e0229b4dbf720b7b0a43000b
JungJubong <wnqhdqhd123@gmail.com>
2024-04-07 15:53:17 +0900

    6kyu:2 - ValidBraces - DeleteNth

---
+++ Algoritm/CodeWars/6Kyu/DeleteNth.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    public class DeleteNth
+    {
+        public static int[] Run(int[] arr, int x)
+        {
+            List<int> list = arr.ToList();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            List<int> result = new List<int>();
+
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (dict.ContainsKey(list[index]))
+                {
+                    dict[list[index]]++;
+                }
+                else
+                {
+                    dict.Add(list[index], 1);
+                }
+
+                if (dict[list[index]] <= x)
+                {
+                    result.Add(list[index]);
+                };
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/ValidBraces.cs
@@ -0,0 +1,71 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    public class ValidBraces
+    {
+        public static bool validBraces(String braces)
+        {
+            char leftSmall = '(';
+            char rightSmall = ')';
+            char leftMedium = '{';
+            char rightMedium = '}';
+            char leftLarge = '[';
+            char rightLarge = ']';
+            string left = "({[";
+            string right = ")}]";
+            
+            Stack<char> stack = new Stack<char>();
+            foreach(char c in braces)
+            {
+                char top;
+
+                if(stack.Any())
+                {
+                    // 여는경우
+                    if (left.Contains(c))
+                    {
+                        stack.Push(c);
+                    }
+                    // 닫는경우
+                    else
+                    {
+                        top = stack.Peek();
+
+                        if (top.Equals(leftSmall) && c.Equals(rightSmall))
+                        {
+                            stack.Pop();
+                            continue;
+                        }
+                        if (top.Equals(leftMedium) && c.Equals(rightMedium))
+                        {
+                            stack.Pop();
+                            continue;
+                        }
+                        if (top.Equals(leftLarge) && c.Equals(rightLarge))
+                        {
+                            stack.Pop();
+                            continue;
+                        }
+
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (right.Contains(c))
+                    {
+                        return false;
+                    }
+                    else 
+                    {
+                        stack.Push(c);
+                        continue;
+                    }
+                }
+            }
+
+            return stack.Any() == false;
+        }
+    }
+}
