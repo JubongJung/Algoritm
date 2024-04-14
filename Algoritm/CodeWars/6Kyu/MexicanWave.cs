bd1af5c10e9fed42c730f3a2720baef080ddbc14
JungJubong <wnqhdqhd123@gmail.com>
2024-04-14 14:25:32 +0900

    6kyu:2 - MexicanWave - WriteNumberExpandedForm

---
+++ Algoritm/CodeWars/6Kyu/MexicanWave.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    public class MexicanWave
+    {
+        public static List<string> wave(string str)
+        {
+            str = str.ToLower();
+            List<string> list = new List<string>();
+            for(int i  = 0; i < str.Length; i++)
+            {
+                char[] strArray = str.ToArray();
+                if (strArray[i].ToString() == " ")
+                {
+                    continue;
+                }
+                strArray[i] = strArray[i].ToString().ToUpper().ToCharArray()[0];
+
+                list.Add(string.Join("", strArray));
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/WriteNumberInExpandedForm.cs
@@ -0,0 +1,22 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class WriteNumberInExpandedForm
+    {
+        public static string ExpandedForm(long num)
+        {
+            string strNumber = num.ToString();
+
+            List<long> list = new List<long>();
+            for(int i = 0; i < strNumber.Length; i++)
+            {
+                long number = Convert.ToInt64(strNumber[i].ToString());
+                if (number > 0)
+                {
+                    list.Add(number * (long)Math.Pow(10, strNumber.Length - i - 1));
+                }
+            }
+
+            return string.Join(" + ", list);
+        }
+    }
+}
