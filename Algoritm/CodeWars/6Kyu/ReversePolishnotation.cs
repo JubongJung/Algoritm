49c1b1ed0f3d778b1de6fc97aa8b255a657a7d8c
JungJubong <wnqhdqhd123@gmail.com>
2024-04-21 18:59:16 +0900

    6kyu:2 - ReversePolishNotaion - WelrDstRiNgCaSe

---
+++ Algoritm/CodeWars/6Kyu/ReversePolishnotation.cs
@@ -0,0 +1,48 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class ReversePolishnotation
+    {
+        public static double Run(string expr)
+        {
+            Stack<double> stack = new Stack<double>();
+            
+            foreach (string element in expr.Split(" "))
+            {
+                if (double.TryParse(element, out double stackElement))
+                {
+                    stack.Push(stackElement);
+                }
+                else
+                {
+                    if(stack.Count < 2)
+                    {
+                        return 0;
+                    }
+
+                    double num2 = stack.Pop();
+                    double num1 = stack.Pop();
+
+                    switch (element)
+                    {
+                        case "+":
+                            stack.Push(num1 + num2);
+                            break;
+                        case "-":
+                            stack.Push(num1 - num2);
+                            break;
+                        case "*":
+                            stack.Push(num1 * num2);
+                            break;
+                        case "/":
+                            stack.Push(num1 / num2);
+                            break;
+                        default:
+                            break;
+                    };
+                }
+            }
+
+            return stack.Pop();
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/WeIrDStRiNgCaSe.cs
@@ -0,0 +1,36 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class WeIrDStRiNgCaSe
+    {
+        public static string Run(string s)
+        {
+            string result = "";
+
+            int index = 1;
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                string charString = s[i].ToString();
+
+                if (charString == " ")
+                {
+                    result += charString;
+                    index = 1;
+                    continue;
+                }
+
+                if(index % 2 == 0)
+                {
+                    result += charString.ToLower();
+                }
+                else
+                {
+                    result += charString.ToUpper();
+                }
+                index++;
+            }
+
+            return result;
+        }
+    }
+}
