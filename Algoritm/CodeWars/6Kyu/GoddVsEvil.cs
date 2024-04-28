473631709140db1dd2cd0cf10317e1a60bf93247
JungJubong <wnqhdqhd123@gmail.com>
2024-04-28 23:27:13 +0900

    6kyu:2 - GoodVsEvil - TortoiseRace

---
+++ Algoritm/CodeWars/6Kyu/GoddVsEvil.cs
@@ -0,0 +1,44 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class GoddVsEvil
+    {
+        public static string Run(string good, string evil)
+        {
+            int[] arrGood = good.Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+            int[] arrEvil = evil.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            int[] goodWorth = { 1, 2, 3, 3, 4, 10 };
+            int[] evilWorth = { 1, 2, 2, 2, 3, 5, 10 };
+
+            string goodWin = "Battle Result: Good triumphs over Evil";
+            string evilWin = "Battle Result: Evil eradicates all trace of Good";
+            string noVictor = "Battle Result: No victor on this battle field";
+
+            int goodSum = 0;
+            int evilSum = 0;
+
+            for(int i = 0; i < arrGood.Length; i++)
+            {
+                goodSum += arrGood[i] * goodWorth[i];
+            }
+
+            for(int i = 0; i < arrEvil.Length; i++)
+            {
+                evilSum += arrEvil[i] * evilWorth[i];
+            }
+
+            if(goodSum > evilSum)
+            {
+                return goodWin;
+            }
+            else if(evilSum > goodSum)
+            {
+                return evilWin;
+            }
+            else
+            {
+                return noVictor;
+            }
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/TortoiseRace.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class TortoiseRace
+    {
+        public static int[] Race(int v1, int v2, int g)
+        {
+            if (v1 >= v2)
+            {
+                return new int[] { -1, -1, -1 };
+            }
+
+            int seconds = (g * 3600) / (v2 - v1);
+
+            int hours = seconds / 3600;
+            int minutes = (seconds % 3600) / 60;
+            int remainingSeconds = seconds % 60;
+
+            return new int[] { hours, minutes, remainingSeconds };
+        }
+    }
+}
