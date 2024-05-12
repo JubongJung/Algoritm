658b589f5b62c8898969ccfc2d7ff5474134b70e
JungJubong <wnqhdqhd123@gmail.com>
2024-05-12 17:49:10 +0900

    6kyu:2 - BouncingBalls - IsPrime

---
+++ Algoritm/CodeWars/6Kyu/BouncingBalls.cs
@@ -0,0 +1,26 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class BouncingBalls
+    {
+        public static int Run(double h, double bounce, double window)
+        {
+            if (h < 0 || bounce >= 1 || bounce <= 0 || window >= h)
+            {
+                return -1;
+            }
+
+            int bounceCount = 0;
+
+            while (h > window)
+            {
+                bounceCount++;
+                h = h * bounce;
+                if (h > window)
+                {
+                    bounceCount++;
+                }
+            }
+            return bounceCount;
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/IsPrime.cs
@@ -0,0 +1,17 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class IsPrime
+    {
+        public static bool Run(int n)
+        {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
