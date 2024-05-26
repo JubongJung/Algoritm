9b173eefb79d1dff49b6bb183fc1eb2907ee2014
JungJubong <wnqhdqhd123@gmail.com>
2024-05-26 20:56:31 +0900

    6kyu:2 - RectangleIntoSquares - ToCamelCase

---
+++ Algoritm/CodeWars/6Kyu/RectangleIntoSquares.cs
@@ -0,0 +1,33 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class RectangleIntoSquares
+    {
+        public static List<int> Run(int lng, int wdth)
+        {
+            if (lng == wdth)
+            {
+                return null;
+            }
+
+            List<int> result = new List<int>();
+
+            while (lng != wdth)
+            {
+                if (lng > wdth)
+                {
+                    result.Add(wdth);
+                    lng -= wdth;
+                }
+                else
+                {
+                    result.Add(lng);
+                    wdth -= lng;
+                }
+            }
+
+            result.Add(lng);
+
+            return result;
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/ToCamelCase.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    internal class ToCamelCase
+    {
+        public static string Run(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            string[] words = str.Split(' ');
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i<words.Length; i++)
+            {
+                var arrWord = words[i].ToCharArray();
+                StringBuilder sb = new StringBuilder();
+                for(int j = 0; j < arrWord.Length; j++)
+                {
+                    if(j == 0)
+                    {
+                        sb.Append(arrWord[j].ToString().ToUpper());
+                    }
+                    else
+                    {
+                        sb.Append(arrWord[j].ToString().ToLower());
+                    }
+                }
+                result.Append(sb.ToString());
+            }
+
+            return result.ToString();
+        }
+    }
+}
