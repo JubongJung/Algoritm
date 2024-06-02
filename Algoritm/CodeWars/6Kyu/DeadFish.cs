98a7814697f63491817bf0ca03c1215aa3c9d3c7
JungJubong <wnqhdqhd123@gmail.com>
2024-06-02 20:01:32 +0900

    6kyu:2 - DeadFish - TitleCase

---
+++ Algoritm/CodeWars/6Kyu/DeadFish.cs
@@ -0,0 +1,35 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class DeadFish
+    {
+        public static int[] Parse(string data)
+        {
+            int value = 0;
+            List<int> result = new List<int>();
+
+            foreach (char command in data)
+            {
+                switch (command)
+                {
+                    case 'i':
+                        value++;
+                        break;
+                    case 'd':
+                        value--;
+                        break;
+                    case 's':
+                        value *= value;
+                        break;
+                    case 'o':
+                        result.Add(value);
+                        break;
+                    default:
+                        // Ignore invalid characters
+                        break;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/TitleCase.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    public class TitleCase
+    {
+        public static string ConvertToTitleCase(string input, string minorWords = "")
+        {
+            string[] words = input.Split(' ');
+
+            words[0] = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(words[0].ToLower());
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                string word = words[i].ToLower();
+                if (!string.IsNullOrEmpty(minorWords) && minorWords.ToLower().Split(' ').Contains(word))
+                    words[i] = word;
+                else
+                    words[i] = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word);
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
