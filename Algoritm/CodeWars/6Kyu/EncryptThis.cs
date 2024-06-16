5b80f3bf336c8a6b6a8e5cd621c0f3b667737b33
JungJubong <wnqhdqhd123@gmail.com>
2024-06-16 21:55:42 +0900

    6kyu:2 - EncryptThis - FindMissing

---
+++ Algoritm/CodeWars/6Kyu/EncryptThis.cs
@@ -0,0 +1,44 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class EncryptThis
+    {
+        public static string Run(string input)
+        {
+            string[] words = input.Split(' ');
+            string[] encryptedWords = new string[words.Length];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+
+                if (string.IsNullOrEmpty(word))
+                {
+                    encryptedWords[i] = word;
+                    continue;
+                }
+
+                char firstChar = word[0];
+                string asciiFirstChar = ((int)firstChar).ToString();
+
+                if (word.Length == 1)
+                {
+                    encryptedWords[i] = asciiFirstChar;
+                }
+                else if (word.Length == 2)
+                {
+                    encryptedWords[i] = asciiFirstChar + word[1];
+                }
+                else
+                {
+                    char secondChar = word[1];
+                    char lastChar = word[word.Length - 1];
+                    string middle = word.Substring(2, word.Length - 3);
+
+                    encryptedWords[i] = asciiFirstChar + lastChar + middle + secondChar;
+                }
+            }
+
+            return string.Join(" ", encryptedWords);
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/FindMissing.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class FindMissing
+    {
+        public static int Run(List<int> list)
+        {
+            if (list == null || list.Count <= 2)
+                return 0;
+
+            int difference = list[1] - list[0];
+
+            for (int i = 2; i < list.Count; i++)
+            {
+                if (list[i] - list[i - 1] != difference)
+                    return list[i - 1] + difference;
+            }
+
+            return 0;
+        }
+    }
+}
