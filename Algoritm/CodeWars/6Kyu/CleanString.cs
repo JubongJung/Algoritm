9cafe8de06b4596d9803330c4aca438a8b841cf1
JungJubong <wnqhdqhd123@gmail.com>
2024-06-23 20:35:13 +0900

    6kyu:2 - CleanString -DataReverse

---
+++ Algoritm/CodeWars/6Kyu/CleanString.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Algoritm.CodeWars._6Kyu
+{
+    public class CleanString
+    {
+        public static string Run(string s)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                if (c == '#')
+                {
+                    if (result.Length > 0)
+                    {
+                        result.Length--; 
+                    }
+                }
+                else
+                {
+                    result.Append(c); 
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/DataReverse.cs
@@ -0,0 +1,28 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class DataReverse
+    {
+        public static int[] Run(int[] data)
+        {
+            int segmentLength = 8;
+            int totalLength = data.Length;
+
+            string bitString = string.Join("", data);
+
+            List<string> segments = new List<string>();
+            for (int i = 0; i < bitString.Length; i += segmentLength)
+            {
+                segments.Add(bitString.Substring(i, segmentLength));
+            }
+
+            segments.Reverse();
+
+            string reversedBitString = string.Join("", segments);
+
+            int[] reversedBits = reversedBitString.Select(c => c - '0').ToArray();
+
+            return reversedBits;
+        }
+
+    }
+}
