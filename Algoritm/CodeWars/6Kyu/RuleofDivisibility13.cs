93604608c72bcdd5d8a0b22b312f95ac5b8c509c
JungJubong <wnqhdqhd123@gmail.com>
2024-06-30 22:54:50 +0900

    6kyu:2 - RuleofDivisibility13 - ValidateCreaditCardNumber

---
+++ Algoritm/CodeWars/6Kyu/RuleofDivisibility13.cs
@@ -0,0 +1,27 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class RuleofDivisibility13
+    {
+        public static long Run(long n)
+        {
+            int[] pattern = { 1, 10, 9, 12, 3, 4 };
+            long sum = 0;
+
+            while (true)
+            {
+                long currentSum = n.ToString()
+                                    .Reverse()
+                                    .Select((digit, index) => (digit - '0') * pattern[index % pattern.Length])
+                                    .Sum();
+
+                if (currentSum == sum)
+                    break;
+
+                sum = currentSum;
+                n = sum;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Algoritm/CodeWars/6Kyu/ValidateCreditCardNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace Algoritm.CodeWars._6Kyu
+{
+    public class ValidateCreditCardNumber
+    {
+        public static bool Run(string n)
+        {
+            n = n.Replace(" ", "");
+
+            int sum = 0;
+            bool doubleDigit = false;
+
+            for (int i = n.Length - 1; i >= 0; i--)
+            {
+                int digit = n[i] - '0';
+
+                if (doubleDigit)
+                {
+                    digit *= 2;
+                    if (digit > 9)
+                    {
+                        digit -= 9;
+                    }
+                }
+
+                sum += digit;
+                doubleDigit = !doubleDigit;
+            }
+
+            return sum % 10 == 0;
+        }
+    }
+}
